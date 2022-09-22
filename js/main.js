9c72ec8ebce12f323c9fb9fa19be9c0447012574
rodrigoacosta46 <roroacostacant@gmail.com>
2022-09-22 12:17:29 -0300

    fix error

--- js/main.js
+++ js/main.js
@@ -19,11 +19,12 @@ function weather(){
                     }
                 document.getElementById('mainCont').innerHTML += "<div class='"+dia+"'><div style='display:"+display+"'class='dayCont'><p>"+new Date(datos['list'][i]['dt_txt']).getHours()+':00'+"</p><div class='dayInf'><div class='row'>&#128167; Humedad "+datos["list"][i]["main"]["humidity"]+"%</div></div><div class='dayInf'><div class='row'><img src='https://cdn-icons-png.flaticon.com/512/3161/3161249.png'>Temperatura "+datos['list'][i]['main']['temp']+"°</div></div><div class='dayInf'><div class='row'><img src='https://cdn-icons-png.flaticon.com/512/4005/4005925.png'> Presion "+datos['list'][i]['main']['pressure']+"hPa</div></div><div class='dayInf'><div class='row'><img src='http://openweathermap.org/img/wn/"+datos['list'][i]['weather'][0]['icon']+".png'> "+datos['list'][i]['weather'][0]['description']+"</div></div></div></div";
             }
-            $('#d'+new Date().getDay()).attr('style', 'background-color: yellow');
             
             for(j=0; j<7; j++){
                 $('#d'+j).attr('onclick', 'day('+j+','+new Date().getDay()+')');
+                $('#d'+j).attr('style', 'background-color: white');
             }
+            $('#d'+new Date().getDay()).attr('style', 'background-color: yellow');
         },
 
         error: function(){
